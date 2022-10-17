105dfe11839f7bfd05b176bd5ea4c814dc94a87d
Ibrahim Khalil Bouafif <ibrahimkhalil.bouafif@esprit.tn>
2022-10-17 22:50:23 +0100

    add mockito tests

---
+++ src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceImplTest.java
@@ -0,0 +1,25 @@
+package tn.esprit.rh.achat.services.produit;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.services.IProduitService;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProduitServiceImplTest {
+	/*
+    @Autowired
+    IProduitService ps;
+    @Test
+    @Order(1)
+    public void testRetrieveAllUsers() {
+        List<Produit> listProduits = ps.retrieveAllProduits();
+        Assertions.assertEquals(0, listProduits.size());
+    }
+*/
+
+}
